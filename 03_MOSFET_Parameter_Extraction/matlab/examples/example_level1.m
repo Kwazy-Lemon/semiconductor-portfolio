e9bd4af99cf3defd06e1a9bac0a330f4bd49f1bd
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 16:31:02 +0800

    Update example_level1.m

--- 03_MOSFET_Parameter_Extraction/matlab/examples/example_level1.m
+++ 03_MOSFET_Parameter_Extraction/matlab/examples/example_level1.m
@@ -1,10 +1,38 @@
-data_transfer = read_ltspice_csv(...);
+clc;
+clear;
+close all;
 
-data_transfer.VDS = ...
-data_transfer.W = ...
-data_transfer.L = ...
-data_transfer.Cox = ...
+%% Add Toolkit
 
-data_output = read_ltspice_step(...);
+addpath(genpath('..'));
+
+%% Read Transfer Characteristics (Id-VGS)
+
+data_transfer = read_ltspice_csv("../../datasets/raw/Id_Vgs.txt");
+
+%% Device Information
+
+data_transfer.VDS = 0.05;
+data_transfer.W   = 10e-6;
+data_transfer.L   = 1e-6;
+data_transfer.Cox = 3.45e-3;
+
+%% Read Output Characteristics (Id-VDS)
+
+data_output = read_ltspice_step("../../datasets/raw/Id_Vds.txt");
+
+%% Level-1 Parameter Extraction
 
 params = level1_extract(data_transfer,data_output);
+
+%% Display Results
+
+fprintf("\n");
+fprintf("=========================================\n");
+fprintf(" Final Results\n");
+fprintf("=========================================\n");
+fprintf("Vth    = %.4f V\n",params.Vth);
+fprintf("mu     = %.4e m^2/Vs\n",params.mu);
+fprintf("lambda = %.4e 1/V\n",params.lambda);
+fprintf("ro     = %.4e Ohm\n",params.ro);
+fprintf("=========================================\n");
