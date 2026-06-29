f8df6e11840d0db14fe4d25e6ae4afdb9986d5ea
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 15:58:30 +0800

    Update read_ltspice_csv.m

--- 03_MOSFET_Parameter_Extraction/matlab/utilities/read_ltspice_csv.m
+++ 03_MOSFET_Parameter_Extraction/matlab/utilities/read_ltspice_csv.m
@@ -1,23 +1,67 @@
 function data = read_ltspice_csv(filename)
-%READ_LTSPICE_CSV Import LTspice transfer curve.
+%READ_LTSPICE_CSV Import LTspice datasets.
+%
+% Description:
+%   Import LTspice simulation data for MOSFET parameter extraction.
+%
+% Inputs:
+%   filename - Path to LTspice output file
+%
+% Outputs:
+%   data - Structure containing imported data
 %
 % Author:
 %   Jianhao Wu
+%
+% Project:
+%   MOSFET Parameter Extraction Toolkit
 
-fprintf('=========================================\n');
-fprintf(' MOSFET Parameter Extraction Toolkit\n');
-fprintf(' LTspice Data Import\n');
-fprintf('=========================================\n');
+fprintf("=========================================\n");
+fprintf(" MOSFET Parameter Extraction Toolkit\n");
+fprintf(" LTspice Data Import\n");
+fprintf("=========================================\n");
 
-T = readtable(filename);
+T = readtable(filename,'VariableNamingRule','preserve');
 
-data.VGS = T{:,1};
-data.IDS = T{:,2};
+data = struct();
 
-data.ColumnNames = T.Properties.VariableNames;
+%% Save Raw Table
+
 data.RawTable = T;
+data.ColumnNames = T.Properties.VariableNames;
 
-fprintf('Import completed.\n');
-fprintf('Samples : %d\n',length(data.VGS));
+%% Detect Dataset Type
+
+[~,name,~] = fileparts(filename);
+
+switch lower(name)
+
+    case "id_vgs"
+
+        data.Type = "Transfer";
+
+        data.VGS = T{:,1};
+        data.IDS = T{:,2};
+
+    case "id_vds"
+
+        data.Type = "Output";
+
+        data.VDS = T{:,1};
+        data.IDS = T{:,2};
+
+    otherwise
+
+        error("Unsupported LTspice dataset.");
+
+end
+
+fprintf("Import completed.\n");
+
+fprintf("Dataset : %s\n",data.Type);
+
+fprintf("Samples : %d\n",height(T));
+
+fprintf("=========================================\n");
 
 end
