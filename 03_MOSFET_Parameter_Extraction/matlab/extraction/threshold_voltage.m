6a686024374fed7b7d51c8695afe69806ffc08f2
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:06:02 +0800

    Update threshold_voltage.m

--- 03_MOSFET_Parameter_Extraction/matlab/extraction/threshold_voltage.m
+++ 03_MOSFET_Parameter_Extraction/matlab/extraction/threshold_voltage.m
@@ -1,1 +1,20 @@
+function Vth = threshold_voltage(data)
+%THRESHOLD_VOLTAGE Extract threshold voltage.
+%
+% Description:
+%   Estimate the threshold voltage from transfer characteristics.
+%
+% Inputs:
+%   data - I-V dataset
+%
+% Outputs:
+%   Vth - Threshold voltage
+%
+% Author:
+%   Jianhao Wu
 
+% To be implemented.
+
+Vth = [];
+
+end
