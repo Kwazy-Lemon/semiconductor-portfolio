bf1f6a08404cacb78f41fcd86ca77b63ef683f60
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:10:49 +0800

    Update threshold_voltage.m

--- 03_MOSFET_Parameter_Extraction/matlab/extraction/threshold_voltage.m
+++ 03_MOSFET_Parameter_Extraction/matlab/extraction/threshold_voltage.m
@@ -1,20 +1,84 @@
-function Vth = threshold_voltage(data)
-%THRESHOLD_VOLTAGE Extract threshold voltage.
+function Vth = threshold_voltage(data, method)
+%THRESHOLD_VOLTAGE Extract threshold voltage from MOSFET transfer characteristics.
 %
 % Description:
-%   Estimate the threshold voltage from transfer characteristics.
+%   Estimate the threshold voltage (Vth) using different extraction
+%   methods from MOSFET transfer characteristics.
 %
 % Inputs:
-%   data - I-V dataset
+%   data    - Structure containing MOSFET I-V data
+%             data.VGS : Gate voltage (V)
+%             data.IDS : Drain current (A)
+%
+%   method  - Extraction method
+%             "linear"            : Linear extrapolation
+%             "constant_current"  : Constant current method
+%             "gm_max"            : Maximum transconductance
 %
 % Outputs:
-%   Vth - Threshold voltage
+%   Vth     - Extracted threshold voltage (V)
 %
 % Author:
 %   Jianhao Wu
+%
+% Repository:
+%   MOSFET Parameter Extraction Toolkit
+%
+% Version:
+%   v1.0
+%
+% Last Updated:
+%   June 2026
 
-% To be implemented.
+%% Default Method
 
-Vth = [];
+if nargin < 2
+    method = "linear";
+end
+
+%% Read Data
+
+VGS = data.VGS;
+IDS = data.IDS;
+
+%% Select Extraction Method
+
+switch lower(method)
+
+    case "linear"
+
+        %--------------------------------------------------------------
+        % Linear Extrapolation Method
+        %--------------------------------------------------------------
+
+        % To be implemented
+
+        Vth = [];
+
+    case "constant_current"
+
+        %--------------------------------------------------------------
+        % Constant Current Method
+        %--------------------------------------------------------------
+
+        % To be implemented
+
+        Vth = [];
+
+    case "gm_max"
+
+        %--------------------------------------------------------------
+        % Maximum Transconductance Method
+        %--------------------------------------------------------------
+
+        % To be implemented
+
+        Vth = [];
+
+    otherwise
+
+        error("Unknown threshold voltage extraction method.");
+
+end
 
 end
