ec070f87f29b0f2c65f49697d4edae8f75f4dd8b
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:07:01 +0800

    Update output_resistance.m

--- 03_MOSFET_Parameter_Extraction/matlab/extraction/output_resistance.m
+++ 03_MOSFET_Parameter_Extraction/matlab/extraction/output_resistance.m
@@ -1,1 +1,20 @@
+function ro = output_resistance(data)
+%OUTPUT_RESISTANCE Estimate output resistance.
+%
+% Description:
+%   Calculate output resistance from saturation-region characteristics.
+%
+% Inputs:
+%   data - I-V dataset
+%
+% Outputs:
+%   ro - Output resistance
+%
+% Author:
+%   Jianhao Wu
 
+% To be implemented.
+
+ro = [];
+
+end
