fc63da2949c23a0b0e7561eb024fccfe7ff9e4ce
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:32:02 +0800

    Create constant_current.m

---
+++ 03_MOSFET_Parameter_Extraction/matlab/extraction/constant_current.m
@@ -0,0 +1,24 @@
+function Vth = constant_current(data,Iref)
+%CONSTANT_CURRENT Threshold voltage extraction using constant current method.
+%
+% Inputs:
+%   data.VGS
+%   data.IDS
+%   Iref (optional)
+%
+% Outputs:
+%   Vth
+
+if nargin < 2
+    Iref = 1e-6;
+end
+
+VGS = data.VGS;
+IDS = data.IDS;
+
+% Find point closest to reference current
+[~, idx] = min(abs(IDS - Iref));
+
+Vth = VGS(idx);
+
+end
