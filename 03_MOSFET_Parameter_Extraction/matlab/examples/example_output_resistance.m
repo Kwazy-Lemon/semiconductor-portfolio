bc4eaddafd136be66e58c4e64c55c38fbef47905
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 16:20:40 +0800

    Update example_output_resistance.m

--- 03_MOSFET_Parameter_Extraction/matlab/examples/example_output_resistance.m
+++ 03_MOSFET_Parameter_Extraction/matlab/examples/example_output_resistance.m
@@ -1,1 +1,18 @@
+clc;
+clear;
+close all;
 
+%% Add Toolkit
+
+addpath(genpath('..'));
+
+%% Read Dataset
+
+data = read_ltspice_step("../../datasets/raw/Id_Vds.txt");
+
+%% Output Resistance
+
+ro = output_resistance(data);
+
+fprintf("\n");
+fprintf("Final Output Resistance = %.4e Ohm\n",ro);
