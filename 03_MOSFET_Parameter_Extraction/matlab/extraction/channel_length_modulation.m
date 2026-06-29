1e3e9861fbe4b055f95ab23fe9f0d9f194278d89
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:06:43 +0800

    Update channel_length_modulation.m

--- 03_MOSFET_Parameter_Extraction/matlab/extraction/channel_length_modulation.m
+++ 03_MOSFET_Parameter_Extraction/matlab/extraction/channel_length_modulation.m
@@ -1,1 +1,20 @@
+function lambda = channel_length_modulation(data)
+%CHANNEL_LENGTH_MODULATION Extract channel-length modulation parameter.
+%
+% Description:
+%   Estimate λ from saturation-region output characteristics.
+%
+% Inputs:
+%   data - I-V dataset
+%
+% Outputs:
+%   lambda - Channel-length modulation parameter
+%
+% Author:
+%   Jianhao Wu
 
+% To be implemented.
+
+lambda = [];
+
+end
