7e61dc467e9901274b11526cd57b2fa4493b333c
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:40:49 +0800

    Update read_ltspice_csv.m

--- 03_MOSFET_Parameter_Extraction/matlab/utilities/read_ltspice_csv.m
+++ 03_MOSFET_Parameter_Extraction/matlab/utilities/read_ltspice_csv.m
@@ -1,49 +1,23 @@
 function data = read_ltspice_csv(filename)
-%READ_LTSPICE_CSV Import LTspice simulation data.
-%
-% Description:
-%   Reads LTspice exported TXT/CSV files and converts them into a MATLAB
-%   structure for parameter extraction and visualization.
-%
-% Inputs:
-%   filename - LTspice output file
-%
-% Outputs:
-%   data - Structure containing imported data
-%
-% Fields:
-%   data.X
-%   data.Y
-%   data.RawTable
+%READ_LTSPICE_CSV Import LTspice transfer curve.
 %
 % Author:
 %   Jianhao Wu
-%
-% Project:
-%   MOSFET Parameter Extraction Toolkit
 
-fprintf('-----------------------------------------\n');
+fprintf('=========================================\n');
 fprintf(' MOSFET Parameter Extraction Toolkit\n');
-fprintf(' LTspice Data Import Module\n');
-fprintf('-----------------------------------------\n');
+fprintf(' LTspice Data Import\n');
+fprintf('=========================================\n');
 
-% Read table
 T = readtable(filename);
 
-% Store original table
+data.VGS = T{:,1};
+data.IDS = T{:,2};
+
+data.ColumnNames = T.Properties.VariableNames;
 data.RawTable = T;
 
-% First column
-data.X = T{:,1};
-
-% Second column
-data.Y = T{:,2};
-
-% Column names
-data.ColumnNames = T.Properties.VariableNames;
-
-fprintf('Import Successful!\n');
-fprintf('Samples : %d\n',height(T));
-fprintf('Columns : %d\n',width(T));
+fprintf('Import completed.\n');
+fprintf('Samples : %d\n',length(data.VGS));
 
 end
