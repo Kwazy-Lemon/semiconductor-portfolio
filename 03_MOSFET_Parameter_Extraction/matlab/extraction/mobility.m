99fec914e045da928b0ca5d1c32d6884e590fd4f
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:06:25 +0800

    Update mobility.m

--- 03_MOSFET_Parameter_Extraction/matlab/extraction/mobility.m
+++ 03_MOSFET_Parameter_Extraction/matlab/extraction/mobility.m
@@ -1,1 +1,20 @@
+function mu = mobility(data)
+%MOBILITY Extract carrier mobility.
+%
+% Description:
+%   Estimate effective carrier mobility using Level-1 equations.
+%
+% Inputs:
+%   data - I-V dataset
+%
+% Outputs:
+%   mu - Carrier mobility
+%
+% Author:
+%   Jianhao Wu
 
+% To be implemented.
+
+mu = [];
+
+end
