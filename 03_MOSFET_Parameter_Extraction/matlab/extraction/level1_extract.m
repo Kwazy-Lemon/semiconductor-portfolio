0062e7d6e45683404e77d5d4c9dd148470fd6736
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 14:05:41 +0800

    Update level1_extract.m

--- 03_MOSFET_Parameter_Extraction/matlab/extraction/level1_extract.m
+++ 03_MOSFET_Parameter_Extraction/matlab/extraction/level1_extract.m
@@ -1,1 +1,24 @@
+function parameters = level1_extract(data)
+%LEVEL1_EXTRACT Main entry of the MOSFET Parameter Extraction Toolkit.
+%
+% Description:
+%   Extract Level-1 MOSFET compact model parameters from LTspice
+%   simulation data.
+%
+% Inputs:
+%   data - Imported I-V dataset
+%
+% Outputs:
+%   parameters - Extracted Level-1 parameters
+%
+% Author:
+%   Jianhao Wu
+%
+% Project:
+%   MOSFET Parameter Extraction Toolkit
 
+% To be implemented.
+
+parameters = [];
+
+end
