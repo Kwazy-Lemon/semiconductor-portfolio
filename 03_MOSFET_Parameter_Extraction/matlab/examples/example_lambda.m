fa1156d893514d1e427ef12259efbd730663e74b
Jianhao-Wu <wujianhao2021@163.com>
2026-06-29 16:09:31 +0800

    Update example_lambda.m

--- 03_MOSFET_Parameter_Extraction/matlab/examples/example_lambda.m
+++ 03_MOSFET_Parameter_Extraction/matlab/examples/example_lambda.m
@@ -2,23 +2,11 @@ clc;
 clear;
 close all;
 
-%% Add Toolkit Folders
-
 addpath(genpath('..'));
 
-%% Read Dataset
-
-data = read_ltspice_csv("../../datasets/raw/Id_Vds.txt");
-
-%% Device Information
-
-data.VGS = 5;          % Gate bias during output characteristics
-
-%% Channel-Length Modulation
+data = read_ltspice_step("../../datasets/raw/Id_Vds.txt");
 
 lambda = channel_length_modulation(data);
 
 fprintf("\n");
-fprintf("=====================================\n");
-fprintf("Lambda = %.4e 1/V\n",lambda);
-fprintf("=====================================\n");
+fprintf("Final Lambda = %.4e 1/V\n",lambda);
